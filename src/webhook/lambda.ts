f35d13b43729da7d99051f0883bb9394636b8d87
adsake001 <101723775+adsake001@users.noreply.github.com>
2022-03-21 16:36:57 +0200

    Update lambda.ts

--- src/webhook/lambda.ts
+++ src/webhook/lambda.ts
@@ -13,7 +13,45 @@ export async function handler(event: APIGatewayProxyEvent) {
   if (!topic) {
     throw new Error('Webhook URL is required as "process.env.WEBHOOK_TOPIC"')
   }
+exports.handler = function(event, context, callback) {
 
+    var operation = event.operation;
+
+    if (event.mailevents) {
+        event.payload.mailevents = event.mailevents;
+    }
+
+    switch (operation) {
+        case 'accepted':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'rejected':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'delivered':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'opened':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'failed':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'clicked':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'unsubscribed':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'complained':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'stored':
+            dynamo.put(event.payload, callback);
+            break;
+        default:
+            callback('Unknown operation: ${operation}');
+    }
   const notification = new SnsNotificationRepository(topic)
   await sendWebhookEvent(event, notification)
 
